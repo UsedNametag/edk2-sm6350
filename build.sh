170ee43b925dca9eeeb83a654db618ae92642f9c
Eero Salovaara <55268479+UsedNametag@users.noreply.github.com>
2022-05-31 16:01:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SM6350/SM6350.dsc -b RELEASE
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SM6350Pkg/SM6350.dsc -b RELEASE
 gzip -c < workspace/Build/SM6350/RELEASE_GCC5/FV/SM6350_UEFI.fd >uefi.img
 cat SM6350.dtb >>uefi.img
 echo > ramdisk
