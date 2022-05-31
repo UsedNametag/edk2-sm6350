94f993ca6219a538edd20247a50c0574f3d662cf
Eero Salovaara <55268479+UsedNametag@users.noreply.github.com>
2022-05-31 12:26:11 +0300

    Update build_debug.sh

--- build_debug.sh
+++ build_debug.sh
@@ -3,8 +3,8 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p GalaxyA72/GalaxyA72_6G.dsc
-gzip -c < workspace/Build/GalaxyA72/DEBUG_GCC5/FV/GALAXYA72_UEFI.fd >uefi.img
-cat a72q.dtb >>uefi.img
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SM6350Pkg/SM6350.dsc
+gzip -c < workspace/Build/SM6350/DEBUG_GCC5/FV/SM6350_UEFI.fd >uefi.img
+cat SM6350.dtb >>uefi.img
 echo > ramdisk
-abootimg --create boot-a72q_DEBUG.img -k uefi.img -r ramdisk
+abootimg --create boot-SM6350_DEBUG.img -k uefi.img -r ramdisk
